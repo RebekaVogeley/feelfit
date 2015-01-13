d7a6947c49388f534f2781bb746c6fdf4ccab656
Tiago Lira <tls2@cin.ufpe.br>
2015-01-13 11:57:57 -0300

    config

--- feelfit/settings.py
+++ feelfit/settings.py
@@ -1,33 +1,16 @@
-"""
-Django settings for feelfit project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
-"""
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'uui*nm3i(p12^9$rlhtz^0cd=$hlw^4q8zm390wn9b3is)#3u('
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 TEMPLATE_DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['guarded-eyrie-5488.herokuapp.com']
 
-
-# Application definition
 
 INSTALLED_APPS = (
     'django.contrib.admin',
@@ -52,19 +35,8 @@ ROOT_URLCONF = 'feelfit.urls'
 
 WSGI_APPLICATION = 'feelfit.wsgi.application'
 
+DATABASES = {'default': dj_database_url.config()}
 
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
 LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'America/Recife'
@@ -74,7 +46,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 STATIC_ROOT = 'staticfiles'
 
@@ -86,29 +57,23 @@ TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates'),
 )
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
-
 STATIC_URL = '/static/'
-
-# Heroku settings
-
-import dj_database_url
-
-DATABASES = {
-    'default':  dj_database_url.config(),
-}
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
 
 STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'static'),
-  )
+)
+
+
+if not os.environ.get('PRODUCTION'):
+    ALLOWED_HOSTS = ['*']
+    DEBUG = True
+    TEMPLATE_DEBUG = True
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
