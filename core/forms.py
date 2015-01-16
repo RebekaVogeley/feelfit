20677bde1e25b5021f1d359a6774102fd4c2f000
Rebeka Vogeley <rrcv@cin.ufpe.br>
2015-01-15 23:20:35 -0300

    novo

---
+++ core/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+
+class ContactFeelfit(forms.Form):
+	
+	 name = forms.CharField(label='Nome', max_length=100)
+    email = forms.EmailField(label='E-mail')
+    message = forms.CharField(
+        label='Mensagem/Dúvida', widget=forms.Textarea
+    )
+	
