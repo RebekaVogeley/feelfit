baa7d2c1af01cd3924985e70e3885cb79b28bf20
Rebeka Vogeley <rrcv@cin.ufpe.br>
2015-01-19 19:35:44 -0300

    sitenovo2

--- core/views.py
+++ core/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 def home(request):
         context = {'texto': 'Seu primeiro projeto Django no Linux/Ubuntu com Sublime Text 3'}
         return render(request, 'index.html', context)
+
+def exercicios(request):
+	return render(request,'exercicios.html')
--- feelfit/urls.py
+++ feelfit/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^$', 'core.views.home', name='home'),
+    url(r'^exercicios/$', 'core.views.exercicios', name='exercicios'),
     url(r'^admin/', include(admin.site.urls)),
 )
