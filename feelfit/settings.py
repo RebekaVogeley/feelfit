85cf6a726544afaac9a2143dd4db8d310a4194c5
Rebeka Vogeley <rrcv@cin.ufpe.br>
2015-01-26 15:47:04 -0300

    novo31

--- feelfit/settings.py
+++ feelfit/settings.py
@@ -63,7 +63,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'feelfit/core/static'),
 )
 
 TEMPLATE_DIRS = (
